71cc1711871f1a4aae34a58dae65ae5356654349
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-05 23:30:21 +0200

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        // Same as view file name.
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel() {Name="",Age=0 };
+            return View(dogVm);
+        }
+        public IActionResult CreateDog(DogViewModel dog)
+        {
+            dogs.Add(dog);
+            return RedirectToAction(nameof(Index));  
+        }
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
